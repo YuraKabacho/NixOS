c329d8f55c45dddc83d4a6b8e5d1cb80349793c8
YuraRiskov <92525117+YuraKabacho@users.noreply.github.com>
2025-01-19 12:25:43 -0800

    Create disko.nix

---
+++ disko.nix
@@ -0,0 +1,38 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        device = "/dev/disk/by-id/some-disk-id";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            MBR = {
+              type = "EF02";
+              size = "1M";
+              priority = 1;
+                  };
+            ESP = {
+              type = "EF00";
+              size = "100M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
