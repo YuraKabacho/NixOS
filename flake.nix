6b33e6c173dfd94ef2577fd83b6a6dd7e536f16d
YuraRiskov <92525117+YuraKabacho@users.noreply.github.com>
2025-08-12 23:04:42 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -16,10 +16,10 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     system = "x86_64-linux";
-    homeStateVersion = "24.11";
+    homeStateVersion = "25.05";
     user = "user";
     hosts = [
-      { hostname = "nixos"; stateVersion = "24.11"; }
+      { hostname = "nixos"; stateVersion = "25.05"; }
     ];
 
     makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
