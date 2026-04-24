f55d2991b2bfabf743b482df02264a3f2dcc8313
YuraKabacho <yraif33@gmail.com>
2026-04-24 16:56:06 +0300

    5

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
 
-    nixosVersion = "25.11";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,6 +15,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let
     system = "x86_64-linux";
+    nixosVersion = "25.11";
     homeStateVersion = nixosVersion;
     user = "user";
     hosts = [
